99589c3de5211be1564cb3d71bbab174c5bc0372
Mikhail <misha@Admins-Mac-mini.local>
2024-03-07 10:18:37 +0300

    Use performance.now instead of Date.now

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,24 @@
 import { useState, useEffect, useLayoutEffect } from 'react'
 
-function getTime(): string {
-  const date = new Date()
-  const time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds()
-  return time
-}
-
-function wait(ms: number) {
-  let start = Date.now(),
-    now = start;
-  while (now - start < ms) {
-    now = Date.now();
-  }
-}
-
 function App() {
 
-  const [renderTime, setRenderTime] = useState(getTime)
+  const [renderTime, setRenderTime] = useState(performance.now())
 
   useEffect(() => {
-    wait(1)
-    let now = getTime()
+    const now = performance.now()
     console.info('useEffect at ', now)
     setRenderTime(now)
   },
     [])
 
   useLayoutEffect(() => {
-    wait(1)
-    let now = getTime()
+    let now = performance.now()
     console.info('useLayoutEffect at', now)
     setRenderTime(now)
   }, [])
 
   useEffect(() => {
-    wait(1)
-    let now = getTime()
+    let now = performance.now()
     console.info('useEffect no deps', now)
   })
 
